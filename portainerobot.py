bcc9e4694dab6b701acfd70fccba5e9428888172
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2024-02-20 18:46:19 +0300

    /get /set /id commands, no file logging, no msg logging

--- portainerobot.py
+++ portainerobot.py
@@ -3,8 +3,8 @@
 import os
 import logging
 import requests
-# from datetime import datetime
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import BotCommand
+from telegram.ext import Updater, CommandHandler
 from dotenv import load_dotenv
 
 # Load the environment variables from the .env file
@@ -21,110 +21,131 @@ event_handler = logging.FileHandler('events.log', mode='a', encoding='utf-8')
 event_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
 event_logger.addHandler(event_handler)
 
-group_chat_logger = logging.getLogger('group_chat_logger')
-group_chat_handler = logging.FileHandler('group_chat.log', mode='a', encoding='utf-8')
-group_chat_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
-group_chat_logger.addHandler(group_chat_handler)
-
-private_chat_logger = logging.getLogger('private_chat_logger')
-private_chat_handler = logging.FileHandler('private_chat.log', mode='a', encoding='utf-8')
-private_chat_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
-private_chat_logger.addHandler(private_chat_handler)
-
-# Retrieve the Telegram Bot API token
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
 
 # Function to get stack information
-def get_stack_info(stack_name, porta=False):
-    # Retrieve the API key and stack ID from the .env file
+def get_stack_info(update, context):
+    # Check if the command is sent from the specific group
+    if str(update.message.chat_id) != CHAT_ID:
+        event_logger.info(f"{update.message.from_user} tried to use the /get command from chat {update.message.chat_id}")
+        return  
+    if len(context.args) == 0:
+      reply_text = "Give me the stack name, e.g. /get dev (or dev2, dev3, stage-eis, reestr-rf) and I'll answer with the <stack name> <frontend version> <backend version>"
+      context.bot.send_message(chat_id=update.message.chat_id, text=reply_text, reply_to_message_id=update.message.message_id)
+      return
+    else:
+      stack_name = context.args[0]
+
+    stack_id = os.getenv(stack_name)
+    porta = stack_name == 'reestr-rf'
     X_API_KEY = os.getenv('X_API_KEY') if not porta else os.getenv('X_API_KEY_PORTA')
     stack_id = os.getenv(stack_name)
-    
-    # URL to make the GET request
     url = f"https://portainer.eis-online.ru/api/stacks/{stack_id}" if not porta else f"https://porta.eis-online.ru/api/stacks/{stack_id}"
-    
-    # Headers for the GET request
     headers = {
         "X-API-Key": X_API_KEY
     }
-    
-    # Log the request
     event_logger.info(f"Request to {url} with stack ID {stack_id}")
-    
-    # Make the GET request
     response = requests.get(url, headers=headers)
-    
-    # Log the response
-    # event_logger.info(f"Response {response.status_code}: {response.text}")
     event_logger.info(f"Response {response.status_code}")
-    
-    # Check if the request was successful
     if response.status_code == 200:
         data = response.json()
-        
-        # Extract the required information
         name = data.get("Name")
         env = data.get("Env", [])
-        
-        # Find the specific environment variable values
         release_frontend = next((item for item in env if item["name"] == "RELEASE_FRONTEND"), {}).get("value", "Not Found")
         release_backend = next((item for item in env if item["name"] == "RELEASE_BACKEND"), {}).get("value", "Not Found")
-        
-        # Format the extracted information
-        info = f"Name: {name}\nRELEASE_FRONTEND: {release_frontend}\nRELEASE_BACKEND: {release_backend}"
-        
-        # Log the extracted information
+        info = f"{name} {release_frontend} {release_backend}"
         event_logger.info(f"Extracted Info: {info}")
-        
-        return info
+        context.bot.send_message(chat_id=update.message.chat_id, text=info, reply_to_message_id=update.message.message_id)  
     else:
-        # Return an error message
-        error_msg = f"Error {response.status_code}: Unable to retrieve information."
+        error_msg = f"Error {response.status_code}: Unable to retrieve information for stack {stack_name}"
         event_logger.error(error_msg)
-        return error_msg
+        context.bot.send_message(chat_id=update.message.chat_id, text=error_msg, reply_to_message_id=update.message.message_id)
 
-# Command handler function
-def handle_message(update, context):
-    # Extract the message text, chat ID, and user information
-    message_text = update.message.text
-    chat_id = update.message.chat_id
-    user = update.message.from_user
-    # Check if the username exists and is not empty
-    username = f"@{user.username}" if user.username else f"{user.first_name}"
-    last_name = user.last_name or ""
-    group_name = f"'{update.message.chat.title}'" if update.message.chat.title else ""
-    
-    # Determine if the message is from a group chat or a private chat and log accordingly
-    if update.message.chat.type == 'group':
-        group_chat_logger.info(f"Group Chat {group_name} Message from {user.first_name} {last_name} [{username}] (ID: {user.id}): {message_text}")
+
+def set_stack_versions(update, context):
+    # Check if the command is sent from the specific group
+    if str(update.message.chat_id) != CHAT_ID:
+        event_logger.info(f"{update.message.from_user} tried to use the /set command from chat {update.message.chat_id}")
+        return
+    if len(context.args) < 3 or not all([context.args[1].isdigit(), context.args[2].isdigit()]):
+        reply_text = "Give me the stack name, frontend version and backend version, e.g. /set dev 10421 10430"
+        context.bot.send_message(chat_id=update.message.chat_id, text=reply_text, reply_to_message_id=update.message.message_id)
     else:
-        private_chat_logger.info(f"Private Message from {user.first_name} {last_name} [{username}] (ID: {user.id}): {message_text}")
-    
-    # Log the received message
-    event_logger.info(f"Received {update.message.chat.type} {group_name} message from {user.first_name} {last_name} [{username}] (ID: {user.id}): {message_text}")
-    
-    # Check if the message starts with the command prefix
-    if message_text.startswith("@portainerobot"):
-        # Extract the command
-        command_parts = message_text.split()
-        if len(command_parts) == 1:
-            context.bot.send_message(chat_id=chat_id, text="Give me the stack name, e.g. @portainerobot dev (or dev2, dev3, stage-eis, reestr-rf)", reply_to_message_id=update.message.message_id)
+        stack_name, front_ver, back_ver = context.args[0], context.args[1], context.args[2]
+        stack_id = os.getenv(stack_name)
+        porta = stack_name == 'reestr-rf'
+        file_url = f"https://portainer.eis-online.ru/api/stacks/{stack_id}/file" if not porta else f"https://porta.eis-online.ru/api/stacks/{stack_id}/file"
+        
+        stack_url = f"https://portainer.eis-online.ru/api/stacks/{stack_id}?endpointId=1" if not porta else f"https://porta.eis-online.ru/api/stacks/{stack_id}?endpointId=2"
+        X_API_KEY = os.getenv('X_API_KEY') if not porta else os.getenv('X_API_KEY_PORTA')
+        headers = {
+            "X-API-Key": X_API_KEY
+        }
+        # Retrieve the current stack file content and environment variables
+        # Fetch stack file content
+        file_response = requests.get(file_url, headers=headers)
+        if file_response.status_code != 200:
+            event_logger.error(f"Failed to get stack file with status code {file_response.status_code}")
+            context.bot.send_message(chat_id=update.message.chat_id, text=f"Failed to get stack file with status code {file_response.status_code}")
+            return
+        stack_file_content = file_response.json()["StackFileContent"]
+
+        # Fetch current stack environment variables
+        env_response = requests.get(stack_url, headers=headers)
+        if env_response.status_code != 200:
+            event_logger.error(f"Failed to get stack environment variables with status code {env_response.status_code}")
+            context.bot.send_message(chat_id=update.message.chat_id, text=f"Failed to get stack environment variables with status code {env_response.status_code}")
+            return
+        env_vars = env_response.json()["Env"]
+
+        # Update the environment variables with the new versions
+        for env_var in env_vars:
+            if env_var["name"] == "RELEASE_FRONTEND":
+                env_var["value"] = front_ver
+            elif env_var["name"] == "RELEASE_BACKEND":
+                env_var["value"] = back_ver
+
+        # Construct the PUT request body
+        put_data = {
+            "StackFileContent": stack_file_content,
+            "Env": env_vars,
+            "Prune": False  # Set to False to not remove services not defined in the stack file
+        }
+
+        # Make the PUT request to update the stack
+        put_response = requests.put(stack_url, headers=headers, json=put_data)
+        if put_response.status_code == 200:
+            event_logger.info(f"Set [{stack_name}]: front: {front_ver}, back: {back_ver}")
+            reply_text = f"Set: {stack_name} {front_ver} {back_ver}"
+            context.bot.send_message(chat_id=update.message.chat_id, text=reply_text, reply_to_message_id=update.message.message_id)
         else:
-            # Extract the stack name
-            _, stack_name = command_parts
-            # Get the stack information
-            info = get_stack_info(stack_name) if stack_name != 'reestr-rf' else get_stack_info(stack_name, porta=True)
-            # Send the message back to the user, citing their message
-            reply_text = f"{username} requested info:\n{info}"
-            context.bot.send_message(chat_id=chat_id, text=reply_text, reply_to_message_id=update.message.message_id)
-    else:
-        # If the message does not start with the command prefix, ignore it
-        pass
+            event_logger.error(f"Failed to update stack versions with status code {put_response.status_code}")
+            context.bot.send_message(chat_id=update.message.chat_id, text=f"Failed to update stack versions with status code {put_response.status_code}", reply_to_message_id=update.message.message_id)
+
+def get_chat_id(update, context):
+    chat_id = update.message.chat_id
+    event_logger.info(f"Chat ID: {chat_id}")
+    context.bot.send_message(chat_id=chat_id, text=f"This chat ID is: {chat_id}", reply_to_message_id=update.message.message_id)
 
 def main():
     updater = Updater(TELEGRAM_API_TOKEN, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+
+    # Define command handlers
+    dp.add_handler(CommandHandler("get", get_stack_info))
+    dp.add_handler(CommandHandler("set", set_stack_versions))
+    dp.add_handler(CommandHandler("id", get_chat_id))
+
+    # Register bot commands with Telegram
+    commands = [
+        BotCommand('get', 'Returns <stack name> <frontend version> <backend version>'),
+        BotCommand('set', 'Sets <stack name> <frontend version> <backend version>'),
+        BotCommand('id', 'Returns this chat ID')
+    ]
+    updater.bot.set_my_commands(commands)
+
+    # Start the Bot
     updater.start_polling()
     updater.idle()
 
